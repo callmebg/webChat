16fe57d1ec4f127e4871129da4ddd126c60849a0
wcb_dell <497649129@qq.com>
2020-03-27 17:20:50 +0800

    就不

--- code/main.py
+++ code/main.py
@@ -8,6 +8,7 @@ import websockets
 import multiprocessing
 from multiprocessing import Process
 from flask import Flask, render_template, request
+from gevent.pywsgi import WSGIServer
 
 IP = '127.0.0.1'
 PORT_WEB = 800
@@ -28,26 +29,35 @@ USERS = {}
 #提供html
 app = Flask(__name__)
 @app.route('/')
-@app.route('/index')
+@app.route('/index.html')
 def index_chat():
     return render_template("index.html", ip=IP, port=PORT_CHAT)
 
+@app.route('/get_ipport')
+def get_ipport():
+    return json.dumps({"ip": IP, "port": PORT_CHAT})
 @app.errorhandler(404)
 def miss(e):
-    return render_template('404.html')
- 
+    return "别瞎搞鸭"
 @app.errorhandler(500)
 def error(e):
-    return render_template('500.html')
+    return "服务器崩了"
+
 def web():
+    '''
+    logger = logging.getLogger()
+    file_handler = logging.FileHandler('web.log')
+    logger.addHandler(file_handler)
+    logger.setLevel(logging.INFO)'''
+
+    logging.basicConfig(format='%(levelname)s: %(message)s',
+                        filename="web.log",
+                        level=logging.INFO)
     app.run(host='0.0.0.0', port=PORT_WEB)
 
 
 #提供聊天的后台
 async def chat(websocket, path):
-    logging.basicConfig(format='%(asctime)s - %(pathname)s[line:%(lineno)d] - %(levelname)s: %(message)s',
-                        filename="chat.log",
-                        level=logging.INFO)
     # 握手
     await websocket.send(json.dumps({"type": "handshake"}))
     async for message in websocket:
@@ -81,19 +91,24 @@ async def chat(websocket, path):
         await asyncio.wait([user.send(message) for user in USERS.values()])
 
 def chat_server():
+    logging.basicConfig(format='%(asctime)s - %(pathname)s[line:%(lineno)d] - %(levelname)s: %(message)s',
+                        filename="chat.log",
+                        level=logging.INFO)
     start_server = websockets.serve(chat, '0.0.0.0', PORT_CHAT)
     asyncio.get_event_loop().run_until_complete(start_server)
     asyncio.get_event_loop().run_forever()
 
 
 if __name__ == "__main__":
+    #俩进程
     multiprocessing.freeze_support()
+    
     p_web = Process(target=web, daemon = True)
     p_web.start()
     p_chat_server = Process(target=chat_server, daemon = True)
     p_chat_server.start()
 
-    print("按下ctrl + c 结束程序。聊天记录将保存在chat.log")
+    print("按下ctrl + c 结束程序。聊天和路由记录将分别保存在chat.log,web.log")
     print("聊天室地址" + IP + ':' + str(PORT_WEB))
     p_web.join()
     p_chat_server.terminate()
